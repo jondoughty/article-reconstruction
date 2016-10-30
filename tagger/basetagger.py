b8e699e8a348fe0467a663194cec98291e008b08
ngarg <ngarg@calpoly.edu>
2016-10-29 17:16:56 -0700

    Added base file for basetagger and junktagger.

--- tagger/basetagger.py
+++ tagger/basetagger.py
@@ -17,8 +17,11 @@ class Issue(object):
 
     def __init__(self, tags_df):
         self.tags_df = tags_df
-        date = None
-        edition = None
+        self.date = None
+        self.edition = None
+
+    def __str__(self):
+        return '%s, %s' %(str(self.date), str(self.edition))
 
 
 def check_tag_exists(issue, tags):
@@ -30,12 +33,27 @@ def check_tag_exists(issue, tags):
     return set(tags).issubset(unique_tags)
 
 
-def get_issues(folder, clear_columns):
+def get_issues(folder='tagged_data', clear_columns=None, clear_tags=None):
     """
     Gets all the Issue objects from the folder. Clears the columns in
     clear_columns in tags_df for each Issue.
     Owner: Nupur Garg
     """
-    folder_name = os.path.abspath(folder)
-    print(folder_name)
-    pass
+    files = []
+    issues = []
+    columns = ["page", "article", "function", "paragraph", "jump", "ad", "text"]
+
+    # Get test files in the folder.
+    foldername = os.path.abspath(folder)
+    for filename in os.listdir(foldername):
+        full_filename = os.path.join(foldername, filename)
+        if os.path.isfile(full_filename) and 'csv' in filename:
+            files.append(full_filename)
+
+    # Create issue objects from the test files.
+    issues = [Issue(pd.read_csv(path, header=1, names=columns)) for path in files]
+
+    # TODO: CLEAR COLUMNS
+    # TODO: CLEAR TAGS
+    
+    return issues
--- tagger/junktagger.py
+++ tagger/junktagger.py
@@ -1,16 +1,19 @@
 # junktagger.py
 # Nupur Garg
 
+import copy
+
 from basetagger import *
 
 
 def tag(issue):
+    issue = copy.deepcopy(issue)
     return issue
 
 
 def main():
-    issues = get_issues(folder='tagged_data',
-                              clear_columns=[''])
+    issues = get_issues(clear_columns=[''], clear_tags=None)
+    print([str(issue) for issue in issues])
 
 
 if __name__ == "__main__":
